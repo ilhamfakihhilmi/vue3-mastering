a3c5f61b05fcbd61fe469278bd5d9800a82d371a
Fakih <ilhamfakih50@gmail.com>
2025-01-15 21:18:53 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  data() {
+    return {
+      name: "John Doe",
+      status: "pending",
+    };
+  },
+};
+</script>
+
+<template>
+  <h1>{{ name }}</h1>
+  <p v-if="status === 'active'">User is Active</p>
+  <p v-else-if="status === 'pending'">User is Pending</p>
+  <p v-else>User is inactive</p>
+</template>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
