a610db5622c67958f193e6ba14a994964c39ad5d
Fakih <ilhamfakih50@gmail.com>
2025-01-15 21:26:21 +0700

    belajar: v-for, v-else-if, v-else dan v-bind

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,8 @@ export default {
     return {
       name: "John Doe",
       status: "pending",
+      tasks: ["Task One", "Task Two", "Task Three"],
+      link: "https://google.com",
     };
   },
 };
@@ -14,6 +16,14 @@ export default {
   <p v-if="status === 'active'">User is Active</p>
   <p v-else-if="status === 'pending'">User is Pending</p>
   <p v-else>User is inactive</p>
+
+  <h3>Tasks:</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">
+      {{ task }}
+    </li>
+  </ul>
+  <a v-bind:href="link">click me for google</a>
 </template>
 
 <style scoped>
