678be57a0ee356052ae578559180fa1b73759158
Fakih <ilhamfakih50@gmail.com>
2025-01-22 13:11:30 +0700

    selesai belajar membuat todolist

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,41 @@
-<script>
-export default {
-  data() {
-    return {
-      name: "John Doe",
-      status: "pending",
-      tasks: ["Task One", "Task Two", "Task Three"],
-      link: "https://google.com",
-    };
-  },
+<script setup>
+import { initCustomFormatter, onMounted, ref } from "vue";
+
+const name = ref("Fakih");
+const status = ref("active");
+const tasks = ref(["Task One", "Task Two", "Task Three"]);
+const newTask = ref();
+
+const toggleStatus = () => {
+  if (status.value === "active") {
+    status.value = "pending";
+  } else if (status.value === "pending") {
+    status.value = "inactive";
+  } else {
+    status.value = "active";
+  }
 };
+
+const addTask = () => {
+  if (newTask.value.trim() !== "") {
+    tasks.value.push(newTask.value);
+    newTask.value = "";
+  }
+};
+
+const deleteTask = (index) => {
+  tasks.value.splice(index, 1);
+};
+
+onMounted(async () => {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+    const data = await response.json();
+    tasks.value = data.map((task) => task.title);
+  } catch (error) {
+    console.info('Error fetching tasks')
+  }
+});
 </script>
 
 <template>
@@ -17,13 +44,23 @@ export default {
   <p v-else-if="status === 'pending'">User is Pending</p>
   <p v-else>User is inactive</p>
 
+  <form @submit.prevent="addTask">
+    <label for="newTask">Add Task</label>
+    <input type="text" id="newTask" name="newTask" v-model="newTask" />
+    <button type="submit">Submit</button>
+  </form>
+
   <h3>Tasks:</h3>
   <ul>
-    <li v-for="task in tasks" :key="task">
-      {{ task }}
+    <li v-for="(task, index) in tasks" :key="task">
+      <span>{{ task }}</span>
+      <button @click="deleteTask(index)">x</button>
     </li>
   </ul>
-  <a v-bind:href="link">click me for google</a>
+
+  <a :href="link" style="cursor: pointer">click me for google</a>
+  <br />
+  <button @:click="toggleStatus">Change Status</button>
 </template>
 
 <style scoped>
@@ -31,3 +68,5 @@ h1 {
   color: red;
 }
 </style>
+
+//35:35
