881a1e18e78c4a9d39a427038fdc15264584f0d1
Fakih <ilhamfakih50@gmail.com>
2025-01-23 14:36:07 +0700

    belajar props

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,13 @@
+<script setup>
+import Navbar from "@/components/Navbar.vue";
+import Hero from "@/components/Hero.vue";
+import HomeCards from "@/components/HomeCards.vue";
+import JobListing from "@/components/JobListings.vue";
+</script>
+
 <template>
-  <h1 class="bg-red-400">Vue Jobs</h1>
+  <Navbar />
+  <Hero />
+  <HomeCards />
+  <JobListing />
 </template>
